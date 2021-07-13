aca1adeb68778f9b14a140ff8bc234e719769572
JayendraB <jbirtharia@gmail.com>
2021-07-13 23:48:48 +0530

    Added Files

--- flight-fare-service/src/main/java/com/boot/demo/FlightFareServiceApplication.java
+++ flight-fare-service/src/main/java/com/boot/demo/FlightFareServiceApplication.java
@@ -3,9 +3,19 @@ package com.boot.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The type Flight fare service application.
+ *
+ * @author JayendraB  Created on 02/07/21
+ */
 @SpringBootApplication
 public class FlightFareServiceApplication {
 
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(FlightFareServiceApplication.class, args);
 	}
---
+++ spring-cloud-netflix-eureka-server/src/test/java/com/boot/demo/SpringCloudNetflixEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudNetflixEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
