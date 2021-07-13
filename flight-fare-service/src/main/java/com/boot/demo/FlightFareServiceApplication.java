aecb87cd8f1f6e7a6e3d8e15d6a491f35450c3ad
JayendraB <jbirtharia@gmail.com>
2021-07-13 23:55:09 +0530

    Added Files

--- flight-fare-service/src/main/java/com/boot/demo/FlightFareServiceApplication.java
+++ flight-fare-service/src/main/java/com/boot/demo/FlightFareServiceApplication.java
@@ -1,7 +1,13 @@
 package com.boot.demo;
 
+import com.boot.demo.dao.FlightFareRepository;
+import com.boot.demo.model.FlightFare;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
 
 /**
  * The type Flight fare service application.
@@ -18,6 +24,24 @@ public class FlightFareServiceApplication {
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(FlightFareServiceApplication.class, args);
+	}
+
+	/**
+	 * Populate initial data command line runner.
+	 *
+	 * @param repository the repository
+	 * @return the command line runner
+	 */
+	@Bean
+	public CommandLineRunner populateInitialData(FlightFareRepository repository) {
+		return args -> {
+			FlightFare fare1 = new FlightFare(123L, "S123",
+					BigDecimal.valueOf(100),"USD");
+			FlightFare fare2 = new FlightFare(124L, "S124",
+					BigDecimal.valueOf(110),"USD");
+			repository.save(fare1);
+			repository.save(fare2);
+		};
 	}
 
 }
